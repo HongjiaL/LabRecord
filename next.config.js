2f60cb3cfa6ab5a490f6e85529f981d155c9df76
Liu Hongjia <402480324@qq.com>
2026-04-02 11:47:57 +0800

    改

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/((?!api|_next|_ipc).*)',
-        destination: '/index.html',
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
